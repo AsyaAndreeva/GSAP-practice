8f43f2b8b68a06dd6a851a826d007321b90d1e83
AsyaAndreeva <asiara032@gmail.com>
2024-09-17 16:47:30 +0300

    Updated FromTo and Timeline animations

--- gsap-starter-demo/src/pages/GsapFromTo.jsx
+++ gsap-starter-demo/src/pages/GsapFromTo.jsx
@@ -1,5 +1,25 @@
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFromTo = () => {
   // TODO: Implement the gsap.fromTo() method
+
+  useGSAP(() => {
+    gsap.fromTo('#red-box', {
+      x:0,
+      roattion: 0,
+      borderRadius: '0%',
+    },
+      {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      borderRadius: '100%',
+      rotation: 360,
+      duration: 2,
+      ease: 'bounce.Out',
+    })
+  }, []);
 
   return (
     <main>
--- gsap-starter-demo/src/pages/GsapTimeline.jsx
+++ gsap-starter-demo/src/pages/GsapTimeline.jsx
@@ -1,5 +1,40 @@
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTimeline = () => {
   // TODO: Implement the gsap timeline
+
+  const timeline = gsap.timeline({
+    repeat: -1, repeatDelay: 1, yoyo: true,
+  });
+
+  useGSAP(() =>{
+    timeline.to('#yellow-box', {
+      x: 250,
+      roattion: 360,
+      borderRadius: '100%',
+      duration: 2,
+      ease: 'black.inOut',
+    })
+
+    timeline.to('#yellow-box',{
+      y: 250,
+      scale: 2,
+      rotation: 360,
+      borderRadius: '100%',
+      duration: 2,
+      ease: 'black.inOut',
+    })
+
+    timeline.to('#yellow-box',{
+      x: 500,
+      scale: 1,
+      roattion: 360, 
+      borderRadius: '8px',
+      duration: 2,
+      ease: 'black.inOut',
+    })
+  }, []);
 
   return (
     <main>
@@ -35,7 +70,13 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button onClick={() => {
+          if(timeline.paused()){
+            timeline.play();
+          }else{
+            timeline.pause();
+          }
+        }}>Play/Pause</button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
