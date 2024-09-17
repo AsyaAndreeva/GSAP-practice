b65f6ae8812d90f31980ab8ed0ab713233f3175d
AsyaAndreeva <asiara032@gmail.com>
2024-09-17 16:54:17 +0300

    Updated stagger animation

--- gsap-starter-demo/src/pages/GsapStagger.jsx
+++ gsap-starter-demo/src/pages/GsapStagger.jsx
@@ -1,5 +1,26 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
+
+  useGSAP(() =>{
+    gsap.to('.stagger-box', {
+      y: 250,
+      rotation: 360,
+      borderRadius: '100%',
+      repeat: -1,
+      yoyo: true,
+      //stagger: 0.5,
+      stagger: {
+        amount: 1.5,
+        grid: [2, 1],
+        axis: 'y',
+        ease: 'circ.inOut',
+        from: 'center',
+      }
+    });
+  },[])
 
   return (
     <main>
